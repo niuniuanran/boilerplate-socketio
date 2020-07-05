5d0063e96480258cd0fa49c21e6c5e6b17bbc0aa
niuniuanran <irene.anran.niu@gmail.com>
2020-07-05 17:40:22 +1200

    Listen for user count on client side

--- public/client.js
+++ public/client.js
@@ -1,18 +1,15 @@
-/*global io*/
-var socket = io();
-
-$( document ).ready(function() {
-
-
-
-  // Form submittion with new message in field with id 'm'
-  $('form').submit(function(){
-    var messageToSend = $('#m').val();
-    //send message to server here?
-    $('#m').val('');
-    return false; // prevent form submit from refreshing page
+$(document).ready(function() {
+  /*global io*/
+  var socket = io();
+  socket.on("user count", function(data) {
+    console.log(`user count: ${data}`);
   });
 
-
-
+  // Form submittion with new message in field with id 'm'
+  $("form").submit(function() {
+    var messageToSend = $("#m").val();
+    //send message to server here?
+    $("#m").val("");
+    return false; // prevent form submit from refreshing page
+  });
 });
